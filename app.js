4f9d79d6022968a1b0c8e318a2742b73a44f058a
Bahersaad676 <bahersaad676@gmail.com>
2019-03-04 13:07:19 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+var selectElem = document.getElementById('select')
+var img = document.getElementById('img');
+var imgSelected = [];
+var clickTimes = 0;
+var clickTimes1 = 0;
+var imgs = document.getElementById('imgs1');
+var imgs1 = document.getElementById('imgs2');
+var imgs2 = document.getElementById('imgs3');
+var times = document.getElementById('times');
+var tm1 = document.getElementById('tm1');
+var tm2 = document.getElementById('tm2');
+
+console.log(imgs2);
+
+console.log(selectElem.value); // print the value of option
+
+console.log(selectElem.selectedIndex); // print index of option
+
+
+selectElem.onchange = function() {
+	if (this.value === "bmw") {
+		imgs.style.display = "block";
+		imgs1.style.display = "none";
+	} else if (this.value === "marsadi") {
+		imgs.style.display = "none";
+		imgs1.style.display = "block";
+	} else {
+		imgs.style.display = "none";
+		imgs1.style.display = "none";
+	}
+	//var p = document.createElement('p');
+	imgs.onclick = function() {
+		clickTimes++;
+		tm1.textContent = clickTimes;
+		//imgs.append(p);
+	}
+
+	imgs1.onclick = function() {
+		clickTimes1++;
+		tm2.textContent = clickTimes1;
+	}
+}
+
+// when one click on the div in another function create elem and ++ it > then display it 
+
+/*
+function we(txt) {
+	var arr = [];
+	var collc = 0;
+for (let i = 0; i < txt.length; i++) {
+	if (txt.charAt(i) > "m" ) {
+		collc++;
+	}
+}
+return (collc + "/" + txt.length).toString();
+}
+
+function we(txt) {
+	var arr = [];
+for (let i = 0; i < txt.length; i++) {
+	if (txt.charAt(i) > "m" ) {
+		arr.push(txt.charAt(i));
+	}
+}
+return (arr.length + "/" + txt.length).toString();
+}
+
+*/
